32f377bfeae92f4cefca52c17a57faa154961f15
Clinton <clinton@goozmo.com>
2016-12-05 21:28:11 -0700

    updates

--- js/sections/es6.section2.variables.js
+++ js/sections/es6.section2.variables.js
@@ -26,7 +26,7 @@
 **/
 console.log('********** Section 2.1 - Let Declaration ***********');
 
-let sectionTitle	= "Classes &amp; Inheritance";
+let sectionTitle	= "Block Scoped Variables - Const & Let";
 
 App.swapTemplate(document.getElementById('main-container'),`
 	<header>
@@ -74,6 +74,14 @@ App.swapTemplate(document.getElementById('main-container'),`
 				</tr>
 			</table>
 		</div>
+		<header>
+			<h4>Using Let</h4>
+			<ul>
+				<li>only accessible from within defining block</li>
+				<li>ins't hoisted, throws an error if called prior to definition</li>
+				<li>reassigning throws an error</li>
+			</ul>
+		</header>
 		<div class="code-block">
 			<col width="1"/>
 			<col width="11"/>
@@ -99,8 +107,8 @@ App.swapTemplate(document.getElementById('main-container'),`
 						<code>
 							function letTest() { <br/>
 								<i class="indent1"></i>// console.log(x);	<br/>
-								// let escapes hoisting rules. <br/>
-								// would throw an error and halt function as expected in most programming languages<br/>
+								<i class="indent1"></i>// let escapes hoisting rules. <br/>
+								<i class="indent1"></i>// would throw an error and halt function as expected in most programming languages<br/>
 								<i class="indent1"></i>let x = 1; // decalred in functions local scope<br/>
 								<i class="indent1"></i>if (true) { <br/>
 									<i class="indent2"></i>let x = 2;  // declared with let, the variables scope is limited to the if local blocks scope and doesn't inherit <br/>
@@ -115,11 +123,107 @@ App.swapTemplate(document.getElementById('main-container'),`
 				</tr>
 			</table>
 		</div>
+		<header>
+			<h3>Let and closures</h3>
+			<p></p>
+		</header>
+		<div class="code-block">
+			<col width="1"/>
+			<col width="11"/>
+			<table>
+				<tr>
+					<td class="gutter">
+						<span>1</span>
+						<span>2</span>
+						<span>3</span>
+						<span>4</span>
+						<span>5</span>
+						<span>6</span>
+						<span>7</span>
+						<span>8</span>
+					</td>
+					<td class="javascript">
+						<code>
+							function closure() { <br/>
+								<i class="indent1"></i>let localVariable		= "my local varable"; 	//	variable decalred in local scope <br/>
+								<i class="indent1"></i>return function() { 							// 	return function with reference to variable <br/>
+									<i class="indent2"></i>console.log(localVariable); 				// use variable declared in the parent function <br/>
+								<i class="indent1"></i>}(); <br/>
+							} <br/>
+							closure(); <br/>
+						</code>
+					</td>
+				</tr>
+			</table>
+		</div>
+	</div>
+	<header>
+		<hgroup>
+			<h3>Using Const</h3>
+		</hgroup>
+	</header>
+	<div class="section">
+		<div class="code-block">
+			<col width="1"/>
+			<col width="11"/>
+			<table>
+				<tr>
+					<td class="gutter">
+						<span>1</span>
+						<span>2</span>
+						<span>3</span>
+					</td>
+					<td class="javascript">
+						<code>
+							// basic usage <br/>
+							const __CONFIG_OPTION_IP__		= "52.46.187.200";
+						</code>
+					</td>
+				</tr>
+			</table>
+		</div>
+		<div class="code-block">
+			<col width="1"/>
+			<col width="11"/>
+			<table>
+				<tr>
+					<td class="gutter">
+						<span>1</span>
+						<span>2</span>
+						<span>3</span>
+						<span>4</span>
+						<span>5</span>
+						<span>6</span>
+						<span>7</span>
+						<span>8</span>
+						<span>9</span>
+						<span>10</span>
+					</td>
+					<td class="javascript">
+						<code>
+							function constTest(){ <br/>
+								<i class="indent1"></i>const __SCOPED_CONFIG_OPTION_IP__		= "52.46.187.202"; <br/>
+							} <br/>
+							constTest(); <br/><br/>
+							
+							const __SCOPED_CONFIG_OPTION_IP__ = "52.46.187.203";	// no error, previous declaration limited by  scope. <br/>
+							function constTest2(){ <br/>
+								<i class="indent1"></i>const __SCOPED_CONFIG_OPTION_IP__ = "52.46.187.204"; <br/>
+								<i class="indent1"></i>console.log(__SCOPED_CONFIG_OPTION_IP__);<br/>
+							} <br/>
+							constTest2(); <br/>
+							console.log(__SCOPED_CONFIG_OPTION_IP__);	// 52.46.187.203 <br/>
+						</code>
+					</td>
+				</tr>
+			</table>
+		</div>
 	</div>
 `);
 
 
 function varTest() {
+	console.log('@varTest()');
 	console.log(x); // undefined.  X is hoisted from succeeding line.
 	var x = 1;
 	if (true) {
@@ -132,6 +236,7 @@ function varTest() {
 varTest();
 
 function letTest() {
+	console.log('@letTest()');
 	// console.log(x);	// let escapes hoisting rules.
 	let x = 1;
 	if (true) {
@@ -145,6 +250,7 @@ letTest();
 
 // while let is defined as block restricted, they still work with closures
 function closure() {
+	console.log('@closure()');
 	let localVariable		= "my local varable"; 	//	variable decalred in local scope 
 	return function() { 							// 	return function with reference to variable
 		console.log(localVariable); 				// use variable declared in the parent function    
@@ -162,22 +268,22 @@ closure();
 	*
 **/
 console.log('********** Section 2.2 - Const Declaration ***********');
+
+// basic usage
 const __CONFIG_OPTION_IP__		= "52.46.187.200";
-console.log(__CONFIG_OPTION_IP__);
-// const __CONFIG_OPTION_IP__ 		= "51.46.187.201"	// error
+// const __CONFIG_OPTION_IP__ 		= "51.46.187.201"	// error - cannot redeclare
 function constTest(){
 	const __SCOPED_CONFIG_OPTION_IP__		= "52.46.187.202";
 }
 constTest();
 
-// reverse scope
-const __SCOPED_CONFIG_OPTION_IP__			= "52.46.187.203";		// no error, previous declaration limited by scope.
+const __SCOPED_CONFIG_OPTION_IP__			= "52.46.187.203";	// no error, previous declaration limited by scope.
+// console.log(__SCOPED_CONFIG_OPTION_IP__);
 function constTest2(){
-	const __SCOPED_CONFIG_OPTION_IP__		= "52.46.187.204";
-	console.log(__SCOPED_CONFIG_OPTION_IP__);
-	const __SCOPED_CONFIG_OPTION_IP_2__		= "52.46.193.108";
+	const __SCOPED_CONFIG_OPTION_IP__		= "52.46.187.204"; // no error
+	console.log(__SCOPED_CONFIG_OPTION_IP__); // 52.46.287.204
 }
 constTest2();
-console.log(__SCOPED_CONFIG_OPTION_IP__);	// not affected by contTest2();
+console.log(__SCOPED_CONFIG_OPTION_IP__);	// 52.46.187.203
 // console.log(__SCOPED_CONFIG_OPTION_IP_2__);	// error
 // const is best used at base scope
--- js/sections/es6.section3.strings.js
+++ js/sections/es6.section3.strings.js
@@ -26,6 +26,81 @@
 **/
 console.log('********** Section 3 - Template Strings ***********');
 
+let sectionTitle	= "Template Strings";
+
+App.swapTemplate(document.getElementById('main-container'),`
+	<header>
+		<hgroup>
+			<h2>${sectionTitle}</h2>
+			<p>Basic Usage</p>
+		</hgroup>
+	</header>
+	<div class="section">
+		<div class="code-block">
+			<col width="1"/>
+			<col width="11"/>
+			<table>
+				<tr>
+					<td class="gutter">
+						<span>1</span>
+						<span>2</span>
+						<span>3</span>
+						<span>4</span>
+						<span>5</span>
+						<span>6</span>
+					</td>
+					<td class="javascript">
+						<code>
+							let name		= "Insight Designs", <br/>
+								<i class="indent1"></i>address	= "2006 Broadway", <br/>
+								<i class="indent1"></i>city		= "Boulder", <br/>
+								<i class="indent1"></i>state	= "CO"; <br/>
+							console.log(\`We are at \$\{name\} \$\{address\} in \$\{city\}, \$\{state\}\`); <br/>
+						</code>
+					</td>
+				</tr>
+			</table>
+		</div>
+		<header>
+			<h4>Using string literals with dot syntax and line breaks</h4>
+		</header>
+		<div class="code-block">
+			<col width="1"/>
+			<col width="11"/>
+			<table>
+				<tr>
+					<td class="gutter">
+						<span>1</span>
+						<span>2</span>
+						<span>3</span>
+						<span>4</span>
+						<span>5</span>
+						<span>6</span>
+						<span>7</span>
+						<span>8</span>
+						<span>9</span>
+						<span>10</span>
+						<span>11</span>
+					</td>
+					<td class="javascript">
+						<code>
+							var Address		= function(address){ <br/>
+								this.name		= address.name; <br/>
+								this.address	= address.address; <br/>
+								this.city		= address.city; <br/>
+								this.state		= address.state; <br/>
+							}
+							let _address = new Address({name:"Insight Designs", address:"2006", city:"Boulder",state:"CO"}); <br/><br/>
+							console.log(\`We are still at \$\{_address.name\} <br/>
+									<i class="indent1"></i>\$\{_address.city\}, <br/>
+										<i class="indent2"></i>\$\{_address.state\}\`); <br/>
+						</code>
+					</td>
+				</tr>
+			</table>
+		</div>
+	</div>`);
+
 let name		= "Insight Designs",
 	address		= "2006 Broadway",
 	city		= "Boulder",
@@ -40,4 +115,6 @@ var Address		= function(address){
 	this.state		= address.state;
 }
 let _address		 = new Address({name:"Insight Designs", address:"2006", city:"Boulder",state:"CO"});
-console.log(`We are still at ${_address.name} ${_address.city}, ${_address.state}`);
+console.log(`We are still at ${_address.name} 
+	${_address.city}, 
+		${_address.state}`);
